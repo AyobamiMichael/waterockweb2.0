eec20ee436c2e1920901f9ef9c3ad1f7db94286d
Ayobami <ayobamiwealth@gmail.com>
2024-07-21 20:11:34 +0100

    update

--- src/barsandresturants/barsandresturantsdashboard.js
+++ src/barsandresturants/barsandresturantsdashboard.js
@@ -1,8 +1,58 @@
-import React from 'react';
 import './barsandresturantsdashboard.css';
 import BarsAndResturantsNavBar from './barnavbar';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function BarsAndResturantsDashBoard () {
+
+  const [barManagerUserName, setBarUsername] = useState('');
+  const[allbars, setAllBars] = useState([]);
+   const[filteredbars, setBarNumberOfViews] = useState([]);
+
+   var newfilteredBars = [];
+   var barNamesList = []; 
+
+   useEffect(() => {
+    const storedBarUsername = localStorage.getItem('barusername');
+     if (storedBarUsername) {
+       setBarUsername(storedBarUsername);
+     }
+      
+   }, []);
+   console.log(barManagerUserName);
+  // Use the username to get all the bars assigned to this username in the db
+ 
+   useEffect(()=>{
+    fetchAllBarsData();
+   }, []);
+
+   const fetchAllBarsData = async () => {
+    try {
+      const response = await axios.get('https://waterockapi.wegotam.com/allbars');
+      setAllBars(response.data);
+      //console.log(response.data);
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+     // console.log(allbars);
+
+    
+  };
+
+  useEffect(()=>{
+    filterBarNumberOfViews();
+    console.log(allbars);
+    },[allbars, barManagerUserName]);
+  
+    const filterBarNumberOfViews = () =>{
+          
+          newfilteredBars = allbars.filter(item => item.barManagerUserName === barManagerUserName);
+         barNamesList =  newfilteredBars.map(item => item.barNumberOfViews);
+         setBarNumberOfViews(barNamesList);
+         console.log(filteredbars);
+  }
+
   return (
     <div className='main'>
       <div className='barandresturantdashboardnavbar'><BarsAndResturantsNavBar/></div>
