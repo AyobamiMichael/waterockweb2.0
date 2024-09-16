5b1491f4da49599d1f1e6627650b396aa5909b4b
Ayobami <ayobamiwealth@gmail.com>
2024-09-16 16:10:17 +0100

    update

--- src/barsandresturants/signinbarmanager.js
+++ src/barsandresturants/signinbarmanager.js
@@ -1,5 +1,157 @@
 import React, { Component } from "react";
 import Navbar from "../homepagenavbar/navbar";
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import icons for show/hide
+import './signinbarmanager.css';
+
+export default class SignInBarManager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uname: "",
+      password: "",
+      checkuname: '',
+      showPassword: false, // State to toggle password visibility
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.togglePasswordVisibility = this.togglePasswordVisibility.bind(this);
+  }
+
+  componentDidMount() {
+    try {
+      fetch("https://waterockapi.wegotam.com/baramanagerdata", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          token: window.localStorage.getItem("token"),
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data, "barmanagerdata");
+          this.setState({ checkuname: data.data });
+          console.log(this.state.checkuname);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { uname, password } = this.state;
+    console.log(uname, password);
+
+    try {
+      fetch("https://waterockapi.wegotam.com/loginbarmanager", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          uname,
+          password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data, "barmanagerlogin");
+          if (data.status === "ok") {
+            alert("login successful");
+            window.localStorage.setItem("token", data.data);
+            window.localStorage.setItem("isLoggedIn", true);
+            window.localStorage.setItem("barusername", this.state.uname);
+            if (this.state.checkuname.uname === this.state.uname) {
+              window.location.href = "/barsandresturantsdashboard";
+            }
+          } else {
+            alert("User not found");
+          }
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.setState({ showPassword: !this.state.showPassword });
+  }
+
+  render() {
+    const { showPassword } = this.state;
+
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="logincontainer">
+          <form onSubmit={this.handleSubmit}>
+            <h3>Sign In</h3>
+
+            <div className="mb-3 username-container">
+              <input
+                type="text"
+                className="form-control usernamesignin"
+                placeholder="Enter user name"
+                onChange={(e) => this.setState({ uname: e.target.value })}
+                required
+              />
+            </div>
+
+            <div className="mb-3 password-container">
+              <input
+                type={showPassword ? "text" : "password"} // Toggle input type based on state
+                className="form-control passwordsignin"
+                placeholder="Enter password"
+                onChange={(e) => this.setState({ password: e.target.value })}
+                required
+              />
+              <span
+                className="password-toggle-icon"
+                onClick={this.togglePasswordVisibility}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
+
+            <div className="mb-3">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label className="custom-control-label" htmlFor="customCheck1">
+                  Remember me
+                </label>
+              </div>
+            </div>
+
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary submitsignin">
+                Submit
+              </button>
+            </div>
+
+            <p className="forgot-password text-right">
+              <a href="/signupbarmanager">Sign Up</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+/*import React, { Component } from "react";
+import Navbar from "../homepagenavbar/navbar";
 import './signinbarmanager.css';
 
 
@@ -147,4 +299,4 @@ export default class SignInBarManager extends Component {
       </div>
     );
   }
-}
+}*/
--- src/barsandresturants/signupbarmanager.js
+++ src/barsandresturants/signupbarmanager.js
@@ -1,6 +1,136 @@
 import React, { Component } from 'react'
 import Navbar from '../homepagenavbar/navbar';
 import './signupbarmanager.css';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import icons for show/hide
+
+export default class SignUpBarManager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: '',
+      lname: '',
+      uname: "",
+      password: "",
+      showPassword: false // State to toggle password visibility
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.togglePasswordVisibility = this.togglePasswordVisibility.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { fname, lname, uname, password } = this.state;
+    console.log(fname, lname, uname, password);
+    fetch("https://waterockapi.wegotam.com/registerbarmanager", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        fname,
+        lname,
+        uname,
+        password,
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "dataManagers");
+        if (data.status === "ok") {
+          alert("Register successful");
+          window.localStorage.setItem("token", data.data);
+          //window.location.href = "./login";
+        } else if (data.error === 'User Exists') {
+          alert("Username not available");
+        }
+      });
+  }
+
+  togglePasswordVisibility() {
+    this.setState({ showPassword: !this.state.showPassword });
+  }
+
+  render() {
+    const { showPassword } = this.state;
+    return (
+      <div className='main'>
+        <Navbar />
+
+        <div className='backgroundImage' style={{ backgroundImage: `url(${'image'})`, height: '850px' }}>
+          <div className='signupcontainer'>
+            <form onSubmit={this.handleSubmit}>
+              <h3>Sign Up</h3>
+
+              <div className="mb-3">
+                <input
+                  type="firstname"
+                  className="form-control firstname"
+                  placeholder="First name"
+                  onChange={(e) => this.setState({ fname: e.target.value })}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="lastname"
+                  className="form-control lastname"
+                  placeholder="Last name"
+                  onChange={(e) => this.setState({ lname: e.target.value })}
+                  required
+                />
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="username"
+                  className="form-control username"
+                  placeholder="User Name"
+                  onChange={(e) => this.setState({ uname: e.target.value })}
+                  required
+                />
+              </div>
+
+              <div className="mb-3 password-container">
+                <input
+                  type={showPassword ? "text" : "password"} // Toggle input type
+                  className="form-control password"
+                  placeholder="Enter password"
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                  required
+                />
+                <span
+                  className="password-toggle-iconsignup"
+                  onClick={this.togglePasswordVisibility}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
+              </div>
+
+              <div className="d-grid">
+                <button type="submit" className="btn btn-primary submit">
+                  Sign Up
+                </button>
+              </div>
+              <p className="forgot-password text-right">
+                Already registered <a href="/signinbarmanager">sign in?</a>
+              </p>
+              <h3>Or Sign Up with email</h3>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+/*import React, { Component } from 'react'
+import Navbar from '../homepagenavbar/navbar';
+import './signupbarmanager.css';
 
 
 export default class SignUpBarManager extends Component {
@@ -115,3 +245,4 @@ export default class SignUpBarManager extends Component {
     )
   }
 }
+*/
